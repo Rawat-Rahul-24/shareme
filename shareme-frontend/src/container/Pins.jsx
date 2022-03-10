355ace8178ce1746bbb30feeee1e9f0185300627
Rahul <rawatr0799@gmail.com>
2022-03-10 21:16:42 +0530

    initial commit

---
+++ shareme-frontend/src/container/Pins.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Feed, PinDetails, Search, CreatePin, Navbar } from "../components";
+
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar
+          user={user}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetails user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Pins;
